a75e5b248da031c4c7fdc2bd8fe5356a73c7eef3
Mfikryyyyyy11 <mfikryhaikal8@gmail.com>
2024-03-07 23:02:44 +0700

    update

--- assignment_3/App.java
+++ assignment_3/App.java
@@ -1,9 +1,26 @@
+// Muhammad Fikry Haikal - 20220040180
+// TI22I
+// OOP
+
 public class App {
     public static void main(String[] args) throws Exception {
+        // Circle
         Circle circleABC = new Circle("Circle ABC", 7.0);
-        Rectangle rectangle = new Rectangle("Rectangle", 7.5, 4.5);
-        
         circleABC.getInfo();
+
+        // Rectangle
+        Rectangle rectangle = new Rectangle("Rectangle", 7.5, 4.5);
         rectangle.getInfo();
+
+        // Triangle
+        Triangle rightTriangle = new Triangle.RightTriangle("Right Triangle", 10, 4);
+        Triangle equilateralTriangle = new Triangle.EquilateralTriangle("Equilateral Triangle", 3);
+        Triangle isoscelesTriangle = new Triangle.IsoscelesTriangle("Isosceles Triangle", 4, 6);
+        Triangle anyTriangle = new Triangle.AnyTriangle("Any Triangle", 4, 3, 2);
+        
+        rightTriangle.getInfo();
+        equilateralTriangle.getInfo();
+        isoscelesTriangle.getInfo();
+        anyTriangle.getInfo();
     }
 }
--- assignment_3/Triangle.java
+++ assignment_3/Triangle.java
@@ -1,6 +1,47 @@
 public class Triangle extends Shape {
-    public Triangle(){
-        
+    protected double side1;
+    protected double side2;
+    protected double side3;
+
+    public Triangle(String name, double side1, double side2, double side3) {
+        setName(name);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+        calculatePerimeter();
+        calculateArea();
     }
-    
+
+    public void calculatePerimeter() {
+        perimeter = side1 + side2 + side3;
+    }
+
+    public void calculateArea() {
+        double s = perimeter / 2;
+        area = Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
+    }
+
+    public static class RightTriangle extends Triangle {
+        public RightTriangle(String name, double base, double height) {
+            super(name, base, height, Math.sqrt(base*base + height*height));
+        }
+    }
+
+    public static class EquilateralTriangle extends Triangle {
+        public EquilateralTriangle(String name, double side) {
+            super(name, side, side, side);
+        }
+    }
+
+    public static class IsoscelesTriangle extends Triangle {
+        public IsoscelesTriangle(String name, double base, double equalSide) {
+            super(name, base, equalSide, equalSide);
+        }
+    }
+
+    public static class AnyTriangle extends Triangle {
+        public AnyTriangle(String name, double side1, double side2, double side3) {
+            super(name, side1, side2, side3);
+        }
+    }
 }
